672a48471b6458ebd5be57d21e2088a00cfa44fe
MyuB <mjycoco0@naver.com>
2022-02-06 20:17:02 +0900

    waiting for token

---
+++ src/redux/modules/reducer.ts
@@ -0,0 +1,12 @@
+import { connectRouter } from "connected-react-router";
+import { combineReducers } from "redux";
+import { History } from "history";
+import auth from "./auth";
+
+const reducer = (history: History<unknown>) =>
+  combineReducers({
+    auth,
+    router: connectRouter(history),
+  });
+
+export default reducer;
